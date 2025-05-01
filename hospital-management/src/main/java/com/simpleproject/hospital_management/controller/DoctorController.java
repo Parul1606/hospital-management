24f61e510f6d0df4ec377dadbf8dfa6a1307375e
Parul Pandey <parulss1606@gmail.com>
2025-05-01 17:48:14 +0530

    implemented getmapping in hospital management spring application

--- hospital-management/src/main/java/com/simpleproject/hospital_management/controller/DoctorController.java
+++ hospital-management/src/main/java/com/simpleproject/hospital_management/controller/DoctorController.java
@@ -1,11 +1,10 @@
 package com.simpleproject.hospital_management.controller;
 
 import com.simpleproject.hospital_management.model.Doctor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,9 +23,30 @@ public class DoctorController {
     //103 -> {103, sanjay,sanjay@gmail.com,cardio,37}
 
     @PostMapping("/save")
-    public String saveDoctor(Doctor doctor){
+    public String saveDoctor(@RequestBody Doctor doctor){
         doctorMapDb.put(doctor.getId(), doctor);
         System.out.println("doctor data is saved : " + doctorMapDb);
         return "Doctor saved successfully";
+    }
+
+    @PostMapping("/saveList")
+    public String saveDoctorList(@RequestBody List<Doctor> doctorList){
+        for(Doctor doctor1:doctorList){
+            doctorMapDb.put(doctor1.getId(), doctor1);
+        }
+        System.out.println("Doctors saved are: " + doctorMapDb);
+        return "doctorList is saved";
+    }
+
+    @GetMapping("/findAll")
+    public Map<Integer, Doctor> getAllDoctors(){
+        return doctorMapDb;
+    }
+
+    //@PathVariable - takes the input in API url path or endpoint
+    @GetMapping("/findById/{id}")
+    public Doctor getAllDoctorById(@PathVariable int id){
+        Doctor doctor = doctorMapDb.get(id);
+        return doctor;
     }
 }
